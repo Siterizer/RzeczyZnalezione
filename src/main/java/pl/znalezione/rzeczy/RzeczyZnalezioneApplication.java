5869fefe0b9661f63d30880bcbf0b0ca6b83976d
siterizer <siterizer@gmail.com>
2020-12-12 13:36:23 +0100

    Initial Commit

---
+++ src/main/java/pl/znalezione/rzeczy/RzeczyZnalezioneApplication.java
@@ -0,0 +1,13 @@
+package pl.znalezione.rzeczy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RzeczyZnalezioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RzeczyZnalezioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/znalezione/rzeczy/RzeczyZnalezioneApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.znalezione.rzeczy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RzeczyZnalezioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
